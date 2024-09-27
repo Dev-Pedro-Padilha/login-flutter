94b169beda86f705fd9e8f754499ad58d4a34cd6
Pedro Gabriel Nunes Padilha <pedro.padilha@perto.com.br>
2024-09-27 18:07:14 -0300

    Autenticação e Tela Home

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,39 +1,128 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'main.dart';
 
 class HomeScreen extends StatelessWidget {
-  final String responseData;
+  final String responseData; // Armazena a resposta da API em formato JSON
 
+  // Construtor da classe HomeScreen, que recebe a resposta da API
   HomeScreen({required this.responseData});
+
+  // Função que retorna a URL da imagem do usuário
+  String getUserImageUrl(user) {
+    // Substitua pela lógica correta para obter a URL da imagem do usuário
+    return '\\\\perto06\\ECQ\\SIMPEQ\\FOTOS\\${user['user']['description']}.png';
+  }
+
+  // Função de logout
+  void logout(BuildContext context) async {
+    // Reinicia a aplicação chamando o main.dart
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => MyApp()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    // Decodifica a resposta JSON para um objeto Dart
     final user = json.decode(responseData);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Resposta da API'),
+        title: Text('Home Screen'), // Título da AppBar
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu), // Ícone do menu
+            onPressed: () {
+              Scaffold.of(context)
+                  .openDrawer(); // Abre o drawer ao clicar no ícone
+            },
+          ),
+        ],
       ),
+      // Drawer que contém as opções do menu
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text(
+                'Menu', // Título do cabeçalho do drawer
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue, // Cor de fundo do cabeçalho
+              ),
+            ),
+            // Opção de logout no menu
+            ListTile(
+              title: Text('Logout'),
+              onTap: () {
+                logout(context); // Chama a função de logout ao clicar
+              },
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.blue[50], // Cor de fundo da tela
       body: user == null
-          ? Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${user!['user']['cn']}-${user!['user']['description']}',
-                    style: TextStyle(fontSize: 18),
+          ? Center(
+              child:
+                  CircularProgressIndicator()) // Mostra um carregando enquanto o usuário não está disponível
+          : Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(
+                      20.0), // Padding interno do container
+                  width: double
+                      .infinity, // Largura do container ocupando todo o espaço disponível
+                  decoration: BoxDecoration(
+                    color: Colors.blue, // Cor de fundo do container
                   ),
-                  Text(
-                    user!['user']['title'],
-                    style: TextStyle(fontSize: 18),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment
+                        .center, // Alinha os itens no centro verticalmente
+                    children: [
+                      ClipOval(
+                        child: Image.asset(
+                          'assets/images/user.png', // Imagem do usuário
+                          //getUserImageUrl(user), // Comentado: URL da imagem do usuário
+                          width: 50.0, // Largura da imagem
+                          height: 50.0, // Altura da imagem
+                          fit: BoxFit.cover, // Cobre o espaço do contêiner
+                        ),
+                      ),
+                      SizedBox(
+                          width: 20.0), // Espaçamento entre a imagem e o texto
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Alinha os textos à esquerda
+                        children: [
+                          Text(
+                            '${user['user']['cn']} - ${user['user']['description']}', // Nome e descrição do usuário
+                            style: TextStyle(fontSize: 13, color: Colors.white),
+                          ),
+                          Text(
+                            user['user']['title'], // Título do usuário
+                            style: TextStyle(fontSize: 13, color: Colors.white),
+                          ),
+                          Text(
+                            user['user']
+                                ['department'], // Departamento do usuário
+                            style: TextStyle(fontSize: 13, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  Text(
-                    user!['user']['department'],
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
-              )),
+                ),
+                // Adicione mais widgets aqui, se necessário
+              ],
+            ),
     );
   }
 }
